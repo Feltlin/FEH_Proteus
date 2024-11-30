dee469a5012fc79267b09ccf8308c4bebe1f9e0c
Feltlin <82766584+Feltlin@users.noreply.github.com>
2024-11-30 01:07:28 -0500

    Implement health bar display logic.

--- Player.h
+++ Player.h
@@ -8,15 +8,19 @@ class Player{
     public:
         FEHImage Image;
         int x, y;
+        int health;
         char role = 'K';
         char direction = 'R';
 
         Player(int x, int y){
             this->x = x;
             this->y = y;
+            health = 100;
         }
 
         void display(){
+
+            //Draw the player.
             switch(role){
                 case 'K':
                     switch(direction){
@@ -44,6 +48,48 @@ class Player{
                     break;
             }
             Image.Draw(x*16, y*16);
+            Image.Close();
+
+            //Draw the health bar.
+            if(health >= 90){
+                Image.Open("./Image/90Health.png");
+            }
+            else if(health >= 80){
+                Image.Open("./Image/80Health.png");
+            }
+            else if(health >= 70){
+                Image.Open("./Image/70Health.png");
+            }
+            else if(health >= 60){
+                Image.Open("./Image/60Health.png");
+            }
+            else if(health >= 50){
+                Image.Open("./Image/50Health.png");
+            }
+            else if(health >= 40){
+                Image.Open("./Image/40Health.png");
+            }
+            else if(health >= 30){
+                Image.Open("./Image/30Health.png");
+            }
+            else if(health >= 20){
+                Image.Open("./Image/20Health.png");
+            }
+            else if(health >= 10){
+                Image.Open("./Image/80Health.png");
+            }
+            else if(health >= 10){
+                Image.Open("./Image/10Health.png");
+            }
+            else if(health > 0){
+                Image.Open("./Image/0Health.png");
+            }
+            //<= 0 dead.
+            else{
+                Image.Open("./Image/0Health.png");
+            }
+            
+            Image.Draw(x*16, (y - 1)*16);
             Image.Close();
         }
 
